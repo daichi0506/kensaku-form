6cc43b00957017ccd64e5317504c1e31b9e83c64
daichi0506 <93575012+daichi0506@users.noreply.github.com>
2022-10-03 10:00:41 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(function () {
+  $('a[href*="#"]').click(function () {
+    var elmHash = $(this).attr('href');
+    var pos = $(elmHash).offset().top;
+    $('body,html').animate({ scrollTop: pos }, 500);
+    return false;
+  });
+});
